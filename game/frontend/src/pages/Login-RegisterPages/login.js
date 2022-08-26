4a05ef96c6c280e53b6035a5b0001b3be9773910
Hionel <104563991+Hionel@users.noreply.github.com>
2022-08-26 18:09:31 +0300

    Transfer register.js to login/register page
    Slide bug

--- game/frontend/src/pages/Login-RegisterPages/login.js
+++ game/frontend/src/pages/Login-RegisterPages/login.js
@@ -5,13 +5,13 @@ const loginForm = document.querySelector('#login');
 const registerForm = document.querySelector('#register');
 const slideButton = document.querySelector('#button_transition');
 
-function register() {
+function register_slide() {
   loginForm.style.left = '-450px';
   registerForm.style.left = '0';
   slideButton.style.left = '102px';
 }
 
-function login() {
+function login_slide() {
   loginForm.style.left = '0';
   registerForm.style.left = '450px';
   slideButton.style.left = '0';
