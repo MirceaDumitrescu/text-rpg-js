f3860d0bdcfffd46930d308461f1bd50b61b33ee
Mircea Dumitrescu <mircea.dumitrescu@3pillarglobal.com>
2022-08-26 18:17:30 +0300

    Code review fixes

--- game/frontend/src/pages/Login-RegisterPages/login.js
+++ game/frontend/src/pages/Login-RegisterPages/login.js
@@ -1,22 +1,28 @@
 
 import { loginUser } from "../../services/loginUser.js";
 
+function register_slide() {
+    loginForm.style.left = '-450px';
+    registerForm.style.left = '0';
+    slideButton.style.left = '102px';
+  }
+
+  function login_slide() {
+    loginForm.style.left = '0';
+    registerForm.style.left = '450px';
+    slideButton.style.left = '0';
+  }
+
+
 const loginForm = document.querySelector('#login');
 const registerForm = document.querySelector('#register');
 const slideButton = document.querySelector('#button_transition');
 
-function register_slide() {
-  loginForm.style.left = '-450px';
-  registerForm.style.left = '0';
-  slideButton.style.left = '102px';
-}
+const loginTab = document.querySelector('#login-tab');
+const registerTab = document.querySelector('#register-tab');
 
-function login_slide() {
-  loginForm.style.left = '0';
-  registerForm.style.left = '450px';
-  slideButton.style.left = '0';
-}
-
+loginTab.addEventListener('click', login_slide);
+registerTab.addEventListener('click', register_slide);
 
 
 const loginUsername = document.querySelector('#username_input__login');
